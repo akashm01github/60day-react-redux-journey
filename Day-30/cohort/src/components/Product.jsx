49a2c1abbc4e38cbb4e7838dcbee97f500166954
akashm01github <akashmukhopadhayay@gmail.com>
2025-12-07 00:46:28 +0530

    Day-30

---
+++ Day-30/cohort/src/components/Product.jsx
@@ -0,0 +1,31 @@
+import { useDispatch } from "react-redux"
+import { addCartItem } from "../store/cartReducer";
+
+export default function Product({productId, id, title, rating, price, image }) {
+
+  const dispatch = useDispatch();
+
+
+  return (
+    <div className="product">
+      <div className="product-image">
+        <img src={image} alt={title} />
+      </div>
+      <div className="title-container">
+        <h3>
+          <a href="#">{title}</a>
+        </h3>
+      </div>
+      <div className="price-rating-container">
+        <p className="rating">{+rating} ★ ★ ★ ★</p>
+        <p className="price">${price}</p>
+      </div>
+      <div className="cta-container">
+        <button onClick={()=>{
+          dispatch(addCartItem({productId, id, title, rating, price, image }));
+        }}>Add to Cart</button>
+        <button>Buy Now</button>
+      </div>
+    </div>
+  )
+}
