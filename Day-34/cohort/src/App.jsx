cfab9df4f89e1b14481d00cca3fd62a8c4379a5b
akashm01github <akashmukhopadhayay@gmail.com>
2025-12-18 22:48:07 +0530

    Day-34

---
+++ Day-34/cohort/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import InfiniteScroolDomApi from './InfiniteScroolDomApi'
+
+const App = () => {
+
+  return (
+    <div className='flex justify-center items-center w-screen h-screen'>
+      <InfiniteScroolDomApi/>
+    </div>
+  )
+}
+
+export default App
---
+++ Day-34/cohort/src/InfiniteScroolDomApi.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+
+const InfiniteScroolDomApi = () => {
+    const [data, setdata] = useState([...new Array(40)]);
+
+    const [laoading, setlaoading] = useState(false);
+
+    const THRESEHOLD = 20;
+
+     const loadMore = () => {
+        setlaoading(true)
+        setTimeout(() => {
+            setdata((prev) => [...prev, ...new Array(10)]);
+            setlaoading(false)
+        }, 3000);
+    }
+
+    function handelScrool(event) {
+        const scrollTop = event.target.scrollTop;
+
+        const clientHeight = event.target.clientHeight;
+
+        const scrollHeight = event.target.scrollHeight;
+
+        const remainingScrool = Math.floor(scrollHeight - (scrollTop+clientHeight));
+
+        
+        
+        console.log(remainingScrool)
+
+        if(remainingScrool < THRESEHOLD && !laoading){
+            loadMore()
+        }
+    }
+
+   
+
+    return (
+        <div onScroll={handelScrool} className='bg-yellow-100 w-[400px]  h-[400px] overflow-y-scroll'>
+            {data.map((row, index) => {
+                return <div className='border-b py-2 text-center' key={index}>
+                    {index + 1}
+                </div>
+            })}
+            {laoading && <div className='text-center text-xl bg-red-200'>Loading...</div>}
+        </div>
+    )
+}
+
+export default InfiniteScroolDomApi
+
