c8195cf3bcb34269588046dc378b95e92ea32a16
akashm01github <akashmukhopadhayay@gmail.com>
2025-12-19 17:53:48 +0530

    Day-34

--- Day-34/cohort/src/InfiniteScrollNew.jsx
+++ Day-34/cohort/src/InfiniteScrollNew.jsx
@@ -1,17 +1,44 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const InfiniteScrollNew = () => {
     const [data, setdata] = useState([...new Array(60)]);
 
+    const [laoading, setlaoading] = useState(false);
+
+    const arrayListRef = useRef([]);
+
+    const loadMore = () => {
+        setlaoading(true)
+        setTimeout(() => {
+            setdata((prev) => [...prev, ...new Array(10)]);
+            setlaoading(false)
+        }, 3000);
+    }   
+
     useEffect(() => {
-        const observer = new IntersectionObserver(handelInterSection)
+        const observer = new IntersectionObserver(function(entires){
+            entires.forEach((entry)=>{
+                if(entry.isIntersecting){
+                    entry.target.style.backgroundColor = 'red';
+                }
+                else{
+                    entry.target.style.backgroundColor = 'blue';
+                }
+            })
+        },{
+                threshold:1
+            });
+
+        arrayListRef.current.forEach((el,index)=>{
+            observer.observe(el);
+        })
     }, [])
 
 
     return (
         <div className='bg-yellow-100 w-100  h-100 overflow-y-scroll'>
             {data.map((row, index) => {
-                return <div className='border-b py-2 text-center' key={index}>
+                return <div ref={(el)=>(arrayListRef.current[index] = el)} className='border-b py-2 text-center' key={index}>
                     {index + 1}
                 </div>
             })}
